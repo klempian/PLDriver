ebeb6dec63612c49a7e1471bb3fc0246db69f5df
Ian Klemp <klemp.ian@gmail.com>
2020-05-16 23:10:03 +0200

    separate configs

--- src/main/java/pl/coderslab/config/RestSecurityConfig.java
+++ src/main/java/pl/coderslab/config/RestSecurityConfig.java
@@ -2,22 +2,26 @@ package pl.coderslab.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.coderslab.security.JWTAuthenticationFilter;
+import pl.coderslab.security.JWTAuthorizationFilter;
 import pl.coderslab.serviceimpl.SpringDataUserDetailsService;
 
 import static pl.coderslab.security.SecurityConstants.SIGN_UP_URL;
 
-@Order(1)
+@Order(2)
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-    public SecurityConfig(SpringDataUserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public RestSecurityConfig(SpringDataUserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
@@ -28,9 +32,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers(SIGN_UP_URL).permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .and().formLogin();
+                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.POST, "/advice/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/advice/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/advice/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/tag/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/tag/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/tag/**").hasRole("ADMIN")
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager(), userDetailsService))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
--- src/main/java/pl/coderslab/controller/HomeController.java
+++ src/main/java/pl/coderslab/controller/HomeController.java
@@ -3,19 +3,13 @@ package pl.coderslab.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-//    @RequestMapping("/")
-//    public String index() {
-//        return "index";
-//    }
-
     @GetMapping("/admin")
     public String admin() {
-        return "admin";
+        return "index";
     }
 
 
